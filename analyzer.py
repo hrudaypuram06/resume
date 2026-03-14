a04382d446e1ab70d953b24c2e98f0afc40b81a2
PURAM HEMA NAGA VENKATA SRI SAI HRUDAY <hrudaypuram06@gmail.com>
2026-03-14 20:36:58 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,51 +1,55 @@
-from job_roles import job_roles
+skills_db = [
+"python","machine learning","data science",
+"sql","html","css","javascript","react",
+"tensorflow","pytorch","deep learning"
+]
 
-all_skills = set()
+roles = {
 
-for role in job_roles:
-    all_skills.update(job_roles[role])
+"Data Scientist":[
+"python","machine learning","data science","sql"
+],
 
+"AI Engineer":[
+"python","tensorflow","pytorch","deep learning"
+],
 
-def analyze_resume(text, job_description=""):
+"Web Developer":[
+"html","css","javascript","react"
+]
+
+}
+
+def analyze_resume(text):
 
     text = text.lower()
 
-    skills = []
+    found_skills = []
 
-    for skill in all_skills:
+    for skill in skills_db:
         if skill in text:
-            skills.append(skill)
+            found_skills.append(skill)
 
-    score = min(len(skills)*10,100)
+    score = min(len(found_skills)*12,100)
 
     best_role = ""
     max_match = 0
     missing = []
 
-    for role, role_skills in job_roles.items():
+    for role, role_skills in roles.items():
 
-        match = len(set(role_skills) & set(skills))
+        match = len(set(role_skills) & set(found_skills))
 
         if match > max_match:
 
             max_match = match
             best_role = role
-            missing = list(set(role_skills) - set(skills))
-
-    jd_match = 0
-
-    if job_description:
-
-        jd_words = job_description.lower().split()
-
-        common = set(jd_words) & set(text.split())
-
-        jd_match = int(len(common)/len(jd_words)*100)
+            missing = list(set(role_skills)-set(found_skills))
 
     suggestions = []
 
     if "project" not in text:
-        suggestions.append("Add project experience")
+        suggestions.append("Add projects section")
 
     if "experience" not in text:
         suggestions.append("Include work experience")
@@ -53,16 +57,11 @@ def analyze_resume(text, job_description=""):
     if "education" not in text:
         suggestions.append("Add education section")
 
-    if score < 50:
-        suggestions.append("Add more technical skills")
-
     return {
 
-        "skills":skills,
+        "skills":found_skills,
         "score":score,
         "role":best_role,
         "missing":missing,
-        "jd_match":jd_match,
         "suggestions":suggestions
-
     }

05749a0f94004fa82eac3e24d0a96d97ec4242c9
PURAM HEMA NAGA VENKATA SRI SAI HRUDAY <hrudaypuram06@gmail.com>
2026-03-14 20:30:41 +0530

    Create job_roles.py

---
+++ job_roles.py
@@ -0,0 +1,19 @@
+job_roles = {
+
+"Data Scientist":[
+"python","machine learning","data science","pandas","numpy","sql"
+],
+
+"AI Engineer":[
+"python","tensorflow","pytorch","deep learning"
+],
+
+"Web Developer":[
+"html","css","javascript","react","node"
+],
+
+"Data Analyst":[
+"excel","sql","python","tableau","power bi"
+]
+
+}
